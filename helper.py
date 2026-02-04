006ade61267cdb682f13a98354ef594967b3a46d
ethanhirshberg <ethirshberg@gmail.com>
2026-02-04 19:08:39 +0200

    7.1.26

--- helper.py
+++ helper.py
@@ -1,36 +1,87 @@
 #קובץ עם פונקציות משותפות
-import streamlit
-from dotenv import load_dotenv
+from dotenv import load_dotenv  #הספריה שפונה לקובץ env
 import os
 import streamlit as st
 from google import genai
+from google.genai import types
+import time
 
-#from pages.homeworkBot import api_key
+def current_time() -> str:
+    """
+    פונקצי שמחזירה מה התאריך והשעה
+    :return:
+    """
+    return time.ctime()
 
-all_models = ["gemini-2.5-flash","gemini-2.0-flash","gemini-2.5-flash-lite","gemini-2.0-flash-lite"]
 
+
+all_models = [
+              "gemini-2.5-flash",
+              "gemini-2.5-flash-lite",
+              "gemini-2.0-flash",
+              "gemini-2.0-flash-lite",
+               "gemini-3-flash",
+               ]
 
 def createClient():
-    st.session_state.client = genai.Client(api_key=loadAPIKey() )
+    st.session_state.client = genai.Client(api_key=loadAPIKey()) #יוצרים לקוח של ג'מיני
 
-def sendMeesage(text,history):
-    if 'client' not in st.session_state:
+def sendMessage(text,system_prompt,history=[]):
+    if 'client' not in st.session_state: #אם לא יצרת חיבור
         createClient()
-    for model in all_models:
+
+    for model in all_models: #עבור על כל המודלים
         client = st.session_state.client
-        try:
-            chat = client.chat.create(
-                model = model
+        try: #מנסה
+            chat = client.chats.create( #יוצר צ'אט
+                model = model,
+                history = history,#ההיסטוריה ששלחנו
+                config = types.GenerateContentConfig (
+                    system_instruction = system_prompt
+                )
             )
-        except:
+
+            ai = chat.send_message(text)  #שליחת הודעה
+            print(ai.text)  #הדפסת תשובה
+            return ai.text #תחזיר את התשובה
+        except Exception as e: #לא הצליח
+            error = str(e) #שומרים את השמירה
+            print(e)
+            if "429" in error: #אם הסיבה היא ששלחתי יותר מדי הודעות
+                st.error("שלחת יותר מדי הודעות, בבקשה לנסות מחר")
+                return #צא
+            if "503" in error: #המודל עמוס
+                st.info(f"המודל עמוס, ננסה מודל אחר")
+            else:
+                st.info("Error: " + error)
+                return
             print(f"{model} not working...")
 
 
-def loadAPIKey():
-    load_dotenv()
-    API_KEY = os.getenv("API_KEY")  or st.secrets["API_KEY"]
+
+def loadAPIKey(): #פונקציה ששולפת את הAPI KEY
+    load_dotenv()  # לטעון את הסביבה
+    API_KEY = os.getenv("API_KEY")  or st.secrets["API_KEY"] # לטעון את המשתנה מהקובץ או מהגדרות האתר
     return API_KEY
 
 def showMessage(sender,text):
-    newMessage = streamlit.chat_message(sender)
-    newMessage.write(text)
+    newMessage = st.chat_message(sender)
+    newMessage.write(text)  # הדמות כותבת
+
+def save_to_history(project,sender,text): #לאיזה פרויקט, מי שלח, מה הוא שלח
+    if project not in st.session_state: #אם לא יצרת את הפרויקט בזיכרון
+        st.session_state[project] = { #צור פרויקט עם היסטוריה ריקה
+            "history": []
+        }
+    st.session_state[project]["history"].append(
+        {
+            "role" : sender,  #מי שלח
+            "parts": [{"text":text }] #הצורה שג'מיני מצפה לקבל בה את ההיטוריה
+        }
+    )
+
+def newPage(project): #פתיחת פרויקט חדש - פועל בפעם הראשונה שנכנסים לדף
+    st.session_state[project] = {  # צור פרויקט עם היסטוריה ריקה
+        "history": []
+    }
+
--- pages/homeworkBot.py
+++ pages/homeworkBot.py
@@ -1,18 +1,63 @@
+from platform import system
+
 import streamlit as st
-from helper import *
+from helper import * #תביא את כל הפונקציות מהקובץ המשותף
 
 st.set_page_config(
-    page_title="בוט שיעורי בית"
+    page_title="בוט שיעורי בית",
+    page_icon="🤓"
 )
 
 st.title("בוט שיעורי בית")
 
-api_key = loadAPIKey()
+api_key = loadAPIKey() #מביא את הפונקציה מהקובץ
 
+#הודעה ראשונה מהצ'אט
+showMessage("AI","היי אני כאן כדאי לעזור לך")
 
-showMessage("AI","היי אני כאן כדי לעזור לך")
+#אם לא הגדרנו את הפרויקט - צור אותו בזיכרון
+if "homework" not in st.session_state:
+    newPage("homework")
 
+system_prompt = """"
+
+#תפקיד
+אתה עוזר בשיעורי בית
+
+#משימה
+התפקיד שלך - להסביר לי את החומר
+אם אתה לא יודע - תגיד שאתה לא יודע או תחפש באינטרנט
+תסביר ברור
+
+#מגבלות
+**לא להמציא תשובה**
+תכתוב תשובה בצורה אנושית
+תכתוב תשובה מלאה
+"""
+
+history = st.session_state["homework"]["history"]
+for line in history:
+    sender = line["role"]
+    if sender == "model": #ג'מיני מצפה לקבל model
+        sender = "ai" #streamlit מצפה לקבל AI
+
+    text = line["parts"][0]["text"] #פשוט מוציאים את הטקסט מהמבנה של ג'מיני
+    showMessage(sender,text)
+
+#מקום להקליד
 user = st.chat_input("ההודעה שלך...")
 
-if user:
+if user: #אם יש הודעה
+
     showMessage("user",user)
+    #שולפים את ההיסטוריה
+
+    save_to_history("homework","user",user)
+    history = st.session_state["homework"]["history"]
+    answer = sendMessage(user,system_prompt,history) #לשלוח לAI את ההודעה
+
+    showMessage("ai",answer) #תראה את התשובה
+
+    save_to_history("homework","model",answer)
+
+
